71f864b2b3f293535cff42d5023fa9ebca951691
Javier Martinez Garcia <79504901+Clonico00@users.noreply.github.com>
2023-03-04 02:08:03 +0100

    updates

---
+++ src/scripts/slider.js
@@ -0,0 +1,29 @@
+const sliderHolder = document.querySelector('.slider__holder');
+const sliderItems = document.querySelectorAll('.slider__item');
+const bullets = document.querySelectorAll('.bullets__item');
+let currentIndex = 0;
+
+function moveTo(index) {
+    if (index < 0 || index >= sliderItems.length || index === currentIndex) {
+        return;
+    }
+
+    sliderHolder.style.transform = `translateX(-${index * 25}%)`;
+    bullets[currentIndex].classList.remove('active');
+    bullets[index].classList.add('active');
+    currentIndex = index;
+}
+
+bullets.forEach((bullet, index) => {
+    bullet.addEventListener('click', () => {
+        moveTo(index);
+    });
+});
+
+setInterval(() => {
+    let newIndex = currentIndex + 1;
+    if (newIndex >= sliderItems.length) {
+        newIndex = 0;
+    }
+    moveTo(newIndex);
+}, 7000);
