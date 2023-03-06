42f1524e623fa6f2cfccc49fca6ab3b11d307087
Javier Martinez Garcia <79504901+Clonico00@users.noreply.github.com>
2023-03-06 20:29:28 +0100

    updates

---
+++ dist/index.6b369942.js
@@ -0,0 +1,26 @@
+const sliders = document.querySelectorAll(".slider");
+sliders.forEach((slider)=>{
+    const sliderHolder = slider.querySelector(".slider__holder");
+    const sliderItems = slider.querySelectorAll(".slider__item");
+    const bullets = slider.querySelectorAll(".bullets__item");
+    let currentIndex = 0;
+    function moveTo(index) {
+        if (index < 0 || index >= sliderItems.length || index === currentIndex) return;
+        sliderHolder.style.transform = `translateX(-${index * 25}%)`;
+        bullets[currentIndex].classList.remove("active");
+        bullets[index].classList.add("active");
+        currentIndex = index;
+    }
+    bullets.forEach((bullet, index)=>{
+        bullet.addEventListener("click", ()=>{
+            moveTo(index);
+        });
+    });
+    setInterval(()=>{
+        let newIndex = currentIndex + 1;
+        if (newIndex >= sliderItems.length) newIndex = 0;
+        moveTo(newIndex);
+    }, 7000);
+});
+
+//# sourceMappingURL=index.6b369942.js.map
